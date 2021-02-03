4d84d8ef5a042b792ed6c0adc5c7a626dc8ac0c2
liux2 <xingbangliu48@gmail.com>
2021-02-03 19:16:28 +0800

    added strctures

---
+++ src/text_analyzation.py
@@ -0,0 +1,2 @@
+def term_frequency():
+    """Calculate the frequency of each word."""
---
+++ src/text_manipulation.py
@@ -0,0 +1,5 @@
+def stop_words_removal():
+    """Remove stop words from texts."""
+
+
+# TODO: lemmatization for English.
